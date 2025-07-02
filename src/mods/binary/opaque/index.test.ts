eaf2276ff534644f96d7c06f12d25b4fe1c3b620
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-07-02 18:15:26 +0900

    d

--- src/mods/binary/opaque/index.test.ts
+++ src/mods/binary/opaque/index.test.ts
@@ -14,8 +14,8 @@ console.log(relative(directory, pathname.replace(".mjs", ".ts")))
 test("Opaque", async ({ test }) => {
   const bytes = new Uint8Array([1, 2, 3, 4, 5])
 
-  const opaque = Readable.readFromBytesOrThrow(Opaque.Copied, bytes)
-  const opaque2 = opaque.readIntoOrThrow(Opaque.Uncopied)
+  const opaque = Readable.readFromBytesOrThrow(Opaque, bytes)
+  const opaque2 = opaque.readIntoOrThrow(Opaque).cloneOrThrow()
   const opaque3 = Opaque.writeFromOrThrow(opaque2)
 
   assert(equals(opaque.bytes, opaque2.bytes))
